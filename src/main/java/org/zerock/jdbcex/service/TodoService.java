e1eddfe7fedf098cc4c7e29132b105c24b4e3cdf
cyberghost0213 <numetalllllll@gmail.com>
2022-09-07 19:01:52 +0900

    220907 Javaweb chapter2

--- src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -1,11 +1,18 @@
 package org.zerock.jdbcex.service;
 
+import com.sun.tools.javac.comp.Todo;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVO;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.util.MapperUtil;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @Log4j2
 public enum TodoService {
@@ -26,6 +33,21 @@ public enum TodoService {
         log.info(todoVO);
 
         dao.insert(todoVO); //int를 반환하므로 이를 이용해서 예외처리도 가능
+    }
+
+    public List<TodoDTO> listAll()throws Exception {
+
+        List<TodoVO> voList = dao.selectAll();
+
+        log.info("voList.......");
+
+        log.info(voList);
+
+        List<TodoDTO> dtoList = voList.stream()
+                .map(vo -> modelMapper.map(vo,TodoDTO.class))
+                .collect(Collectors.toList());
+
+        return dtoList;
     }
 }
 
